16fefd4e8ff6941a8ddc18a100a7e3e8e7d0acab
xuandat <gintoriki@gmail.com>
2024-07-29 13:50:00 +0700

    U-Net

--- U-Net/utils.py
+++ U-Net/utils.py
@@ -76,9 +76,7 @@ def check_accuracy(loader, model, device="cuda"):
                     (preds + y).sum() + 1e-8
             )
 
-    print(
-        f"Got {num_correct}/{num_pixels} with acc {num_correct / num_pixels * 100:.2f}"
-    )
+    print(f"Got {num_correct}/{num_pixels} with acc {num_correct / num_pixels * 100:.2f}")
     print(f"Dice score: {dice_score / len(loader)}")
     model.train()
 
